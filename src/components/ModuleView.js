89c0213a84c853a46df9a478c8ea39707355d384
John Erick Madduma <madduma.johnerick@gmail.com>
2019-08-10 14:54:44 +0800

    view

--- src/components/ModuleView.js
+++ src/components/ModuleView.js
@@ -2,19 +2,23 @@ import React, { useEffect, useState } from "react";
 
 import { ModuleService } from "../services/moduleService";
 
-export default function ModuleView() {
-  const [module, setModule] = useEffect(() => {
+export default function ModuleView(props) {
+  const [moduleItem, setModuleItem] = useState(null);
+  const { match } = props;
+  const moduleId = match.params.moduleId;
+
+  useEffect(() => {
     getModule();
   }, []);
 
   async function getModule() {
     try {
-      const data = await ModuleService.getModule();
-      setModule(data);
+      const data = await ModuleService.getModule(moduleId);
+      setModuleItem(data);
     } catch (err) {
       console.log("ERROR", err);
     }
   }
 
-  return <div />;
+  return <>{moduleItem ? <div>{moduleItem.title}</div> : "Loading..."}</>;
 }
