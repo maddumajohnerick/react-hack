f7bcbadbdf1ad9bee72543228ac0c5aace894d8e
Roie Ambulo <roieambulo@gmail.com>
2019-08-10 12:45:52 +0800

    Fix App.sass design

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Course() {
-  return <div>Course View Here</div>;
+  return (
+    <div className="columns">
+      <div className="course__image column">
+        <img src="https://via.placeholder.com/250" alt="" />
+      </div>
+      <div className="course__content column is-three-quarters">
+        <h2 className="course__title title">Title</h2>
+        <div className="course__description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quis
+            mollitia perspiciatis esse provident culpa earum! Laborum, omnis
+            numquam modi aut quibusdam quasi consequuntur, cumque, accusamus
+            amet distinctio soluta molestias!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,24 +1,8 @@
 import React from "react";
+import Course from "./Course";
 
 function Courses() {
-  return (
-    <div className="course__container">
-      <div className="course__image column">
-        <img src="https://via.placeholder.com/250" alt="" />
-      </div>
-      <div className="course__content column">
-        <h2 className="course__title title">Title</h2>
-        <div className="course__description">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quis
-            mollitia perspiciatis esse provident culpa earum! Laborum, omnis
-            numquam modi aut quibusdam quasi consequuntur, cumque, accusamus
-            amet distinctio soluta molestias!
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  return <Course />;
 }
 
 export default Courses;
