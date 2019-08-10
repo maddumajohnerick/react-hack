bb7613e68b1eded3c536dfc7a0727a20fb887caf
John Erick Madduma <madduma.johnerick@gmail.com>
2019-08-10 14:03:51 +0800

    module file

---
+++ src/services/moduleService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const url = "https://my-json-server.typicode.com/oscarmiguel13/modules/db";
+
+class Service {
+  getModules = id => {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url)
+        .then(resp => {
+          resolve(resp.data.all.filter(d => d.course_id == id));
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  };
+
+  getModule = id => {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url)
+        .then(resp => {
+          resolve(resp.data.all.filter(d => d.course_id === id));
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  };
+}
+
+export const ModuleService = new Service();
