f6dd2a7fb4aaf24386666e5f615a4ae1786f15a8
John Erick Madduma <madduma.johnerick@gmail.com>
2019-08-10 13:17:53 +0800

    course details

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Course({ course }) {
   return (
-    <div className="columns">
+    <div className="course__container columns">
       <div className="course__image column">
-        <img src="https://via.placeholder.com/250" alt="" />
+        <Link to={`/course/${course.id}`}>
+          <img src={course.image} alt="" />
+        </Link>
       </div>
       <div className="course__content column is-three-quarters">
-        <h2 className="course__title title">{course.title}</h2>
+        <Link to={`/course/${course.id}`}>
+          <h2 className="course__title title">{course.title}</h2>
+        </Link>
         <div className="course__description">
           <p>{course.description}</p>
         </div>
